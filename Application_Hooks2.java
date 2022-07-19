e68d389f7d661c56fcaa4b3a7bc07ec8cd95e859
SaiMadhuri <saimadhuri2220@gmail.com>
2022-07-19 17:01:10 +0530

    Third Commmit

---
+++ Application_Hooks2.java
@@ -0,0 +1,70 @@
+package AppHooks;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Properties;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import com.qa.factory.Driver_Factory;
+import com.qa.util.ConfigReader;
+import com.qa.util.Generic_Functions;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Application_Hooks2 {
+	
+	private Driver_Factory driverFactory;
+	private WebDriver driver;
+	Properties prop;
+	public static Scenario scenario;
+
+
+	@Before()
+	public void launchBrowser() {
+		driverFactory = new Driver_Factory();
+		try {
+			Application_Hooks2.scenario=scenario;
+			driver = driverFactory.init_driver(ConfigReader.readBrowser());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	/*@AfterStep
+	public void as(Scenario scenario) throws Exception 
+	{
+		
+	    scenario.attach(Generic_Functions.takeFullPageScreenShotAsByte(), "image/png", "screenshots");
+	}*/
+	
+		
+		@After(order = 0)
+	public void quitBrowser() {
+		driver.quit();
+	}
+
+
+	
+	
+	@After(order = 1)
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			// take screenshot:
+			String screenshotName = scenario.getName().replaceAll(" ", "_");
+			byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(sourcePath, "image/png", screenshotName);
+
+		}
+		
+		}
+
+
+}
