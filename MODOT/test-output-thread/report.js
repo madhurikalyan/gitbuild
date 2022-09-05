bd0520da61d91be5b5c890b97908fb674d5390d8
SaiMadhuri <saimadhuri2220@gmail.com>
2022-09-05 22:50:36 +0530

    final

---
+++ MODOT/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"52c7b99a-480f-4aae-8096-6bf2b187dd47","feature":"Renew Fleet feature","scenario":"IRP Renew fleet","start":1662397082127,"group":1,"content":"","tags":"","end":1662397938017,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
