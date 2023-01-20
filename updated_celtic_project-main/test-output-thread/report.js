a2021133daf3e3f245031104853d93671fe455c6
SaiMadhuri <52196651+madhurikalyan@users.noreply.github.com>
2023-01-20 10:19:43 +0530

    del

--- updated_celtic_project-main/test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"def81e74-780c-4c48-9fbc-a7d7fae59cfe","feature":"Renew Fleet feature","scenario":"IRP Renew fleet","start":1668757075055,"group":1,"content":"","tags":"","end":1668757319704,"className":"failed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
